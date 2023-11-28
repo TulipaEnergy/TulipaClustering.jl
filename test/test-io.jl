669b60572d05f6e2f612c917e8fad2ba6ab1914d
Greg Neustroev <G.Neustroev@tudelft.nl>
2023-11-28 10:40:02 +0100

    Add unit tests for clustering data reader

---
+++ test/test-io.jl
@@ -0,0 +1,18 @@
+@testset "Input validation" begin
+  @testset "Make sure that input validation fails for bad files" begin
+    dir = joinpath(INPUT_FOLDER, "bad")
+    @test_throws CSV.Error TulipaClustering.read_csv_with_schema(
+      joinpath(dir, "demand.csv"),
+      TulipaClustering.DemandData,
+    )
+  end
+
+  @testset "Make sure that input files are read into dataframes" begin
+    @test begin
+      dir = joinpath(INPUT_FOLDER, "EU")
+      clustering_data = TulipaClustering.read_clustering_data_from_csv_folder(dir)
+      size(clustering_data.demand) == (175200, 3) &&
+        size(clustering_data.generation_availability) == (525600, 4)
+    end
+  end
+end
