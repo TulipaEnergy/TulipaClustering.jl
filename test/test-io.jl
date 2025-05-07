3001121fdda9a91f3466b444eb1500b3a303819b
Greg Neustroev <G.Neustroev@tudelft.nl>
2025-05-07 11:49:01 +0200

    add year to the test

--- test/test-io.jl
+++ test/test-io.jl
@@ -3,12 +3,21 @@
     dir = joinpath(OUTPUT_FOLDER, "temp")
 
     profile_names = ["solar", "wind"]
+    years = [2030, 2040]
     timeframe_duration = 20
 
     profiles = DataFrame(;
-      profile_name = repeat(profile_names; inner = timeframe_duration),
-      timestep = repeat(1:timeframe_duration; outer = length(profile_names)),
-      value = rand(length(profile_names) * timeframe_duration),
+      profile_name = repeat(
+        profile_names;
+        inner = timeframe_duration,
+        outer = length(years),
+      ),
+      timestep = repeat(
+        1:timeframe_duration;
+        outer = length(profile_names) * length(years),
+      ),
+      value = rand(length(profile_names) * timeframe_duration * length(years)),
+      year = repeat(years; inner = timeframe_duration * length(profile_names)),
     )
 
     num_rep_periods = 3
