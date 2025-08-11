d7b9ec02192b0f0e2fe533ed6c61926232b947ba
Diego Alejandro Tejada Arango <diego.tejadaarango@tno.nl>
2025-08-11 14:32:43 +0200

    Update combine_periods! tests with dataframe layout

--- test/test-cluster.jl
+++ test/test-cluster.jl
@@ -1,5 +1,5 @@
 @testset "Period combination" begin
-  @testset "Make sure that combining perids returns a correct data frame" begin
+  @testset "Make sure that combining periods returns a correct data frame" begin
     @test begin
       df = DataFrame([:period => [1, 1, 2], :timestep => [1, 2, 1], :value => 1:3])
       TulipaClustering.combine_periods!(df)
@@ -8,14 +8,26 @@
     end
   end
 
-  @testset "Make sure that combining perids fails when there is no time step in the data frame" begin
+  @testset "Make sure that combining periods returns a correct data frame using layout" begin
+    @test begin
+      df = DataFrame([:periods => [1, 1, 2], :time_step => [1, 2, 1], :value => 1:3])
+      TulipaClustering.combine_periods!(
+        df;
+        layout = DataFrameLayout(; timestep = :time_step, period = :periods),
+      )
+
+      size(df) == (3, 2) && df.time_step == collect(1:3) && df.value == collect(1:3)
+    end
+  end
+
+  @testset "Make sure that combining periods fails when there is no time step in the data frame" begin
     @test_throws DomainError begin
       df = DataFrame([:period => [1, 1, 2], :value => 1:3])
       TulipaClustering.combine_periods!(df)
     end
   end
 
-  @testset "Make sure that combining perids does nothing when there are time steps but no periods" begin
+  @testset "Make sure that combining periods does nothing when there are time steps but no periods" begin
     @test begin
       df = DataFrame([:timestep => [1, 2], :value => 1:2])
       TulipaClustering.combine_periods!(df)
