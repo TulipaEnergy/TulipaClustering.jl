1b7197a80bbee6d430a9f36102fedd3c966abf55
Diego Alejandro Tejada Arango <diego.tejadaarango@tno.nl>
2025-09-14 18:59:36 +0200

    Update utils functions for testing

--- test/utils.jl
+++ test/utils.jl
@@ -2,6 +2,7 @@ function _new_connection(;
   profile_names = ["name001", "name002"],
   num_timesteps::Int = 24,
   database_schema = "",
+  layout = TulipaClustering.ProfilesTableLayout(),
 )
   @assert length(profile_names) > 0
   @assert num_timesteps ≥ 1
@@ -16,12 +17,63 @@ function _new_connection(;
     connection,
     "CREATE TABLE $(prefix)profiles AS
     SELECT
-      profile_name,
-      i AS timestep,
-      i * 3.14 AS value,
+      $(layout.profile_name) AS $(layout.profile_name),
+      i AS $(layout.timestep),
+      i * 3.14 AS $(layout.value),
+      --$(layout.default_year) AS $(layout.year),
+      --$(layout.default_scenario) AS $(layout.scenario),
     FROM generate_series(1, $num_timesteps) AS s(i)
     CROSS JOIN (
-      SELECT unnest([$profile_names_str]) AS profile_name,
+      SELECT unnest([$profile_names_str]) AS $(layout.profile_name),
+    )
+    ",
+  )
+
+  return connection
+end
+
+function _new_connection_multi_scenario_year(;
+  profile_names = ["name001", "name002"],
+  num_timesteps::Int = 24,
+  years = [2020, 2021],
+  scenarios = [1, 2],
+  database_schema = "",
+  layout = TulipaClustering.ProfilesTableLayout(),
+)
+  @assert length(profile_names) > 0
+  @assert num_timesteps ≥ 1
+  @assert length(years) > 0
+  @assert length(scenarios) > 0
+
+  connection = DBInterface.connect(DuckDB.DB)
+  profile_names_str = join(["'$x'" for x in profile_names], ", ")
+  years_str = join(years, ", ")
+  scenarios_str = join(scenarios, ", ")
+
+  prefix = ""
+  if database_schema != ""
+    DuckDB.query(connection, "CREATE SCHEMA $database_schema")
+    prefix = "$database_schema."
+  end
+
+  DuckDB.query(
+    connection,
+    "CREATE TABLE $(prefix)profiles AS
+    SELECT
+      $(layout.profile_name) AS $(layout.profile_name),
+      i AS $(layout.timestep),
+      (i * 3.14 + $(layout.year) * 0.1 + $(layout.scenario) * 0.01) AS $(layout.value),
+      $(layout.year) AS $(layout.year),
+      $(layout.scenario) AS $(layout.scenario),
+    FROM generate_series(1, $num_timesteps) AS s(i)
+    CROSS JOIN (
+      SELECT unnest([$profile_names_str]) AS $(layout.profile_name),
+    )
+    CROSS JOIN (
+      SELECT unnest([$years_str]) AS $(layout.year),
+    )
+    CROSS JOIN (
+      SELECT unnest([$scenarios_str]) AS $(layout.scenario),
     )
     ",
   )
